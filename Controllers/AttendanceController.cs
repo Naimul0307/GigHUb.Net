a42e55a72260e78595e54e6ed2cbb875006796a8
Naim0307 <mdnaimul15902@gmail.com>
2020-06-27 00:43:02 +0600

    Implementing Rest Api

---
+++ Controllers/AttendanceController.cs
@@ -0,0 +1,41 @@
+﻿using GigHub.Dto;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendanceController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public AttendanceController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
+                return BadRequest("The attend already exists.");
+
+            var attendence = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+            };
+
+            _context.Attendances.Add(attendence);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
