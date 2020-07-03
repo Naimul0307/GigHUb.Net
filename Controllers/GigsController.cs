7b0d2333e6b0ff1f2e3a70a102bc00d8f561b07d
Naim0307 <mdnaimul15902@gmail.com>
2020-07-04 02:30:05 +0600

    Implement My Upcoming Gigs Page

--- Controllers/GigsController.cs
+++ Controllers/GigsController.cs
@@ -19,6 +19,18 @@ namespace GigHub.Controllers
         {
             _context = new ApplicationDbContext();
         }
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Gigs
+                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
+                .Include( g => g.Genre)
+                .ToList();
+            
+            return View(gigs);
+
+        }
 
         [Authorize]
         public ActionResult Attending()
@@ -46,7 +58,7 @@ namespace GigHub.Controllers
                 Genres = _context.Genres.ToList()
             };
 
-            return View(viewModel);
+            return View("Create", viewModel);
         }
 
         [Authorize]
@@ -69,7 +81,7 @@ namespace GigHub.Controllers
             };
             _context.Gigs.Add(gig);
             _context.SaveChanges();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
         }
     }
 }
